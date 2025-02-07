0bcf9015884eade5ec82382b74da7fe7131557ec
Kang <kgh.hyeon@gmail.com>
2025-02-07 15:36:08 +0900

    Update 깃허브.py

--- 깃허브.py
+++ 깃허브.py
@@ -38,14 +38,14 @@ GITHUB_IMAGE_URL = "https://raw.githubusercontent.com/kgh-kang/Test/refs/heads/m
 with st.container():
     # 이미지 중앙 정렬 및 크기 조절
     st.markdown(
-        f"<p style='text-align: center; margin-top: 150px;>?'><img src='{GITHUB_IMAGE_URL}' width='130'></p>",
+        f"<p style='text-align: center; margin-top: 50px; margin-bottom: 70px;>?'><img src='{GITHUB_IMAGE_URL}' width='130'></p>",
         unsafe_allow_html=True
     )
     # :흰색_확인_표시: 제목과 서브타이틀에서 폰트를 직접 지정
     st.markdown("""
         <p style='text-align: center; font-family: "Noto Sans KR", sans-serif; line-height: 1.5;'>
-            <span style="font-size: 30px; font-weight: bold;">노후장비 개인구매</span><br>
-            <span style="font-size: 18px;">네꺼에서 내꺼로 만들 마지막 기회.</span>
+            <span style="font-size: 60px; font-weight: bold;">노후장비 개인구매</span><br>
+            <span style="font-size: 60px; color: #66666D;">네꺼에서 내꺼로 만들 마지막 기회.</span>
         </p>
     """, unsafe_allow_html=True)
     # :흰색_확인_표시: 구매 신청 버튼을 화면 정중앙에 배치
@@ -57,8 +57,8 @@ with st.container():
     # 안내 문구 중앙 정렬
     st.markdown("""
         <p style='text-align: center; font-family: "Noto Sans KR", sans-serif; line-height: 1.5;'>
-            <span style="font-size: 15px; font-weight: 400;">시간이 좀 더 필요하신가요?</span><br>
-            <span style="font-size: 15px; font-weight: 400;">신규 장비 수령 후 2주가 지나면 구매 기회가 사라집니다</span>
+            <span style="font-size: 30px; font-weight: 400; color: #66666D;">시간이 좀 더 필요하신가요?</span><br>
+            <span style="font-size: 30px; font-weight: 400; color: #66666D;">신규 장비 수령 후 2주가 지나면 구매 기회가 사라집니다</span>
         </p>
     """, unsafe_allow_html=True)
 # :흰색_확인_표시: 상단 툴바 숨기기
