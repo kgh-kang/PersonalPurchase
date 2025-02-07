94872eb1c70338fc6b0b6055af0855811acf1c8e
Kang <kgh.hyeon@gmail.com>
2025-02-07 14:40:24 +0900

    Update 깃허브.py

--- 깃허브.py
+++ 깃허브.py
@@ -41,15 +41,14 @@ with st.container():
     )
 
     # ✅ 제목과 서브타이틀에서 폰트를 직접 지정
-    st.markdown("""
-        <p style='text-align: center; font-family: "Noto Sans KR", sans-serif;'>
-            <span style="font-size: 35px; font-weight: bold;">노후장비 개인구매</span>
-            &nbsp;|&nbsp;
-            <span style="font-size: 18px; color: #66666D;">네꺼에서 내꺼로 만들</span>
-            &nbsp;|&nbsp;
-            <span style="font-size: 18px; color: #66666D;">마지막 기회</span>
-        </p>
-    """, unsafe_allow_html=True)
+st.markdown("""
+    <p style='text-align: center; font-family: "Noto Sans KR", sans-serif; line-height: 1.5;'>
+        <span style="font-size: 35px; font-weight: bold;">노후장비 개인구매</span><br>
+        <span style="font-size: 18px; color: #66666D;">네꺼에서 내꺼로 만들</span><br>
+        <span style="font-size: 18px; color: #66666D;">마지막 기회</span>
+    </p>
+""", unsafe_allow_html=True)
+
 
     # ✅ 구매 신청 버튼 (가운데 정렬 + 크기 자동 조정)
     col1, col2, col3 = st.columns([2, 1, 2])
