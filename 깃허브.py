cd181fab05d3ac198265ef7a0d02f8dbaafbd545
Kang <kgh.hyeon@gmail.com>
2025-02-07 14:40:41 +0900

    Update 깃허브.py

--- 깃허브.py
+++ 깃허브.py
@@ -41,13 +41,13 @@ with st.container():
     )
 
     # ✅ 제목과 서브타이틀에서 폰트를 직접 지정
-st.markdown("""
-    <p style='text-align: center; font-family: "Noto Sans KR", sans-serif; line-height: 1.5;'>
-        <span style="font-size: 35px; font-weight: bold;">노후장비 개인구매</span><br>
-        <span style="font-size: 18px; color: #66666D;">네꺼에서 내꺼로 만들</span><br>
-        <span style="font-size: 18px; color: #66666D;">마지막 기회</span>
-    </p>
-""", unsafe_allow_html=True)
+    st.markdown("""
+        <p style='text-align: center; font-family: "Noto Sans KR", sans-serif; line-height: 1.5;'>
+            <span style="font-size: 35px; font-weight: bold;">노후장비 개인구매</span><br>
+            <span style="font-size: 18px; color: #66666D;">네꺼에서 내꺼로 만들</span><br>
+            <span style="font-size: 18px; color: #66666D;">마지막 기회</span>
+        </p>
+    """, unsafe_allow_html=True)
 
 
     # ✅ 구매 신청 버튼 (가운데 정렬 + 크기 자동 조정)
